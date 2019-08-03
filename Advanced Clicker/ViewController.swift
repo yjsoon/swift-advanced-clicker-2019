2804b0c4a73536e22ed1d1f6f173da42ded63bfe
YJ Soon <yjsoon@tinkertanker.com>
2019-08-03 09:03:43 +0800

    Added timer

--- Advanced Clicker/ViewController.swift
+++ Advanced Clicker/ViewController.swift
@@ -12,16 +12,23 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var label: UILabel!
     var counter = 0
+    var time = 0.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(update), userInfo: nil, repeats: true)
     }
     
+    @objc func update() {
+        time += 0.1
+    }
+
     @IBAction func tapped(_ sender: Any) {
         counter += 1
         label.text = "\(counter)"
         if counter == 30 {
             print("Done")
+            print("Time taken: \(time)")
         }
     }
     
