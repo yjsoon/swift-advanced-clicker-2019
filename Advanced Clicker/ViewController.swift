b64a07aa40daaa9651cf246fe8da1115b7abc643
YJ Soon <yjsoon@tinkertanker.com>
2019-08-03 08:53:41 +0800

    Initial Commit

---
+++ Advanced Clicker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Advanced Clicker
+//
+//  Created by Soon Yin Jie on 3/8/19.
+//  Copyright © 2019 Tinkercademy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
